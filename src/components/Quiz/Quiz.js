ef0ffa65caa70d393fc6e42bb82ef937b321d9d3
Devonte MacGlashan <devontem@yahoo-inc.com>
2017-12-09 09:22:00 -0800

    updated quiz container & moved json to public directory

--- src/components/Quiz/Quiz.js
+++ src/components/Quiz/Quiz.js
@@ -1,1 +1,7 @@
-Quiz.js
+import React from 'react';
+
+const Quiz = (props) => {
+	return <h1>hi!</h1>
+}
+
+export default Quiz;
--- src/containers/QuizContainer/QuizContainer.js
+++ src/containers/QuizContainer/QuizContainer.js
@@ -1,9 +1,40 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+import Quiz from './../../components/Quiz/Quiz';
+import { connect } from 'react-redux';
+
+const mapStateToProps = (state, ownProps) => {
+	return {
+		state
+	}
+};
+
+const mapDispatchToProps = (dispatch, ownProps) => {
+	return {
+		getQuestions: ()=> {
+			dispatch({
+				type: 'GET_QUESTIONS',
+				payload: axios.get('/questions.json')
+			})
+		},
+		getAnswers: ()=> {
+			dispatch({
+				type: 'GET_ANSWERS',
+				payload: axios.get('/answers.json')
+			})
+		}
+	}
+};
 
 class QuestionsContainer extends Component {
+	componentWillMount(){
+		console.log(this.props);
+		this.props.getQuestions();
+	}
+
 	render(){
-		return (<h1>test</h1>);
+		return <Quiz {...this.props} />
 	}
 }
 
-export default QuestionsContainer;
+export default connect(mapStateToProps, mapDispatchToProps)(QuestionsContainer);
