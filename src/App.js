879e6a652bcdf34c6f263caeb27d064ec4b79cf8
Devonte MacGlashan <devontem@yahoo-inc.com>
2017-12-09 09:07:44 -0800

    added and updated components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import React, { Component } from 'react';
+import { Provider } from 'react-redux';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import './App.css';
+import QuizContainer from './containers/QuizContainer/QuizContainer';
+import store from './containers/store';
 
 class App extends Component {
   render() {
     return (
-      <h1>Hi World</h1>
+      <Provider store={store}>
+        <BrowserRouter>
+          <Switch>
+            <Route exact path="/" component={QuizContainer} />
+          </Switch>
+        </BrowserRouter>
+      </Provider>
     );
   }
 }
